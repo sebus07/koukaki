7df5f5199dca0653d16186cdffae72e1a3dd9e8a
SEBASTIEN KOENIG <sebastien.koenig@orange.fr>
2024-01-08 08:09:52 +0100

    instalation scss et background du lieu

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -4,6 +4,11 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
+function enqueue_custom_styles() {
+    wp_enqueue_style('nom-du-style', get_stylesheet_directory_uri() . '/css/style.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
+
 // Get customizer options form parent theme
 if ( get_stylesheet() !== get_template() ) {
     add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
@@ -20,3 +25,6 @@ function ajouter_script_personnalise() {
     wp_enqueue_script('script_js', get_stylesheet_directory_uri() . '/js/mon_script.js', array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'ajouter_script_personnalise');
+
+
+
--- wp-content/themes/foce-child/js/mon_script.js
+++ wp-content/themes/foce-child/js/mon_script.js
@@ -1,4 +1,3 @@
-
 // mon_script.js
 
 ///// remplacement de la balise banner en header video///////
@@ -41,4 +40,100 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+/// creation du slider personnages ///
 
+let sliderPersonnage = document.querySelector("figure img");
+console.log(sliderPersonnage);
+let divSlider = document.querySelector("#characters");
+console.log(divSlider);
+let titreSectionPersonnages = document.querySelector(".main-character h3" )
+console.log(titreSectionPersonnages);
+
+
+let imageIndex = 0;
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Masquer les anciennes images
+    let images = document.querySelectorAll('img[alt="Kawaneko"]');
+    if (images.length > 0) {
+        images.forEach(function(image) {
+            image.style.display = "none";
+
+            // Trouver le <figcaption> correspondant et le masquer
+            let figcaption = image.nextElementSibling;
+            if (figcaption && figcaption.tagName === 'FIGCAPTION') {
+                figcaption.style.display = "none";
+            }
+        });
+    }
+
+    // Sélectionner le parent des images existantes
+    let parentContainer = document.querySelector('.other-characters');
+
+    // Vérifiez si l'élément existe avant de tenter de le masquer
+    if (parentContainer) {
+        // Pour masquer l'élément avec la classe "other-characters"
+        parentContainer.style.display = 'none';
+        // Ajouter la flex-direction en colonne
+        parentContainer.style.flexDirection = 'column';
+    }
+
+    // Créer une nouvelle div pour le slider
+    let sliderContainer = document.createElement('div');
+    sliderContainer.classList.add('slider-container');
+
+    // Ajouter une disposition en ligne (horizontal) avec Flexbox
+    sliderContainer.style.display = 'flex';
+    divSlider.style.flexDirection = 'column';
+
+    // Définir la largeur et la hauteur de l'image
+    sliderPersonnage.setAttribute("width", "400");
+    sliderPersonnage.setAttribute("height", "400");
+    sliderContainer.setAttribute("width", "1266");
+    sliderContainer.setAttribute("height", "442");
+    // Ajuster la justification du texte à gauche
+    titreSectionPersonnages.style.textAlign = "left";
+    
+
+    // Ajuster l'image de fond à gauche
+    titreSectionPersonnages.style.backgroundPositionX = "3%";
+    
+    
+
+    // Remplacer chaque figure avec les nouvelles images
+    const newImages = [
+        "wp-content/uploads/2022/06/Kawaneko.png",
+        "wp-content/uploads/2022/06/Orenjiiro-1.png", 
+        "wp-content/uploads/2022/06/Pinku-1.png",
+        "wp-content/uploads/2022/06/Tenshi-1.png",
+        "wp-content/uploads/2022/06/jaakuna-1.png"
+    ];
+
+    for (let i = 0; i < newImages.length; i++) {
+        let figure = document.createElement('figure');
+        let img = document.createElement('img');
+        let figcaption = document.createElement('figcaption');
+
+        img.src = newImages[i];
+        figcaption.textContent = "Caption " + (i + 1); // Vous pouvez personnaliser le texte du caption ici
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+
+        sliderContainer.appendChild(figure);
+    }
+
+    // Insérer la nouvelle div avec la classe slider-container après l'élément .other-characters
+    parentContainer.insertAdjacentHTML('afterend', sliderContainer.outerHTML);
+});
+
+
+
+//////changement background image lieu/////
+    let imageArticle =document.querySelector("#place");
+    console.log(imageArticle);
+
+    imageArticle.style.background = "url('wp-content/uploads/2024/01/Studio_koukaki-image_place.png";
+
+    //ajout des nuage//
+    
