334e68fa71275a447465ae6440623bd733a6fb7b
SEBASTIEN KOENIG <sebastien.koenig@orange.fr>
2024-01-24 10:44:37 +0100

    animation titres finalisé

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -7,7 +7,7 @@ get_header();
         <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?>" alt="logo Fleurs d'oranger & chats errants">
     </section>
     <section id="story" class="story">
-        <h2><span id="gliding-text">L'histoire</span></h2>
+        <h2><div class="hidden"><span class="premier" id="gliding-text"><div class="hidden"><span class="premier">L'</span><span class="deuxieme">histoire</span></div></div></h2>
         <article id="" class="story__article">
             <p><?php echo get_theme_mod('story'); ?></p>
         </article>
@@ -24,7 +24,7 @@ get_header();
 
 <article id="characters">
   <div class="main-character title-container">
-    <h3 class="pers"><span>les </span><span>personnages</span></h3>
+    <h3 class="pers"><div class="hidden"><span class="premier">les </span><span class="deuxieme">personnages</span></div></h3>
     <div class="swiper mySwiper">
       <div class="swiper-wrapper">
         <div class="swiper-slide"><img src="wp-content/uploads/2022/06/Kawaneko.png" alt="Slide 1"></div>
@@ -39,7 +39,7 @@ get_header();
 </article>
         <article id="place">
             <div>
-                <h3><span>Le</span><span> Lieu</span></h3>
+                <h3><div class="hidden"><span class="premier">Le</span><span class="deuxieme"> Lieu</span></div></h3>
                 
                 <p><?php echo get_theme_mod('place'); ?></p>
             </div>
@@ -49,7 +49,7 @@ get_header();
     </section>
 
     <section id="studio">
-        <h2><span>Studio</span><span> Koukaki</span></h2>
+        <h2><div class="hidden"><span class="premier">Studio</span><span class="deuxieme"> Koukaki</span></div></h2>
         <div>
             <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
             <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
--- wp-content/themes/foce-child/js/mon_script.js
+++ wp-content/themes/foce-child/js/mon_script.js
@@ -43,11 +43,37 @@ let monTitreH2Lieu = document.querySelector(".place h3"); // Remplacez "monTitre
 monTitreH2Lieu.classList.add("lieu");
 
 
+$(document).ready(function() {
+  $(window).on('scroll', function() {
+    // Pour chaque balise avec la classe "premier"
+    $('.premier').each(function() {
+      let offset = $(this).offset().top;
+      let scroll = $(window).scrollTop();
+      let windowHeight = $(window).height();
 
+      // Si la balise est visible à l'écran
+      if (scroll > offset - windowHeight + 100) {
+        $(this).addClass('animate-premier');
+      } else {
+        $(this).removeClass('animate-premier');
+      }
+    });
 
+    // Pour chaque balise avec la classe "deuxieme"
+    $('.deuxieme').each(function() {
+      let offset = $(this).offset().top;
+      let scroll = $(window).scrollTop();
+      let windowHeight = $(window).height();
 
-
-
+      // Si la balise est visible à l'écran
+      if (scroll > offset - windowHeight + 50) {
+        $(this).addClass('animate-deuxieme');
+      } else {
+        $(this).removeClass('animate-deuxieme');
+      }
+    });
+  });
+});
 // script.js
 
 
